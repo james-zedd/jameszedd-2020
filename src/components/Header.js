0ef14c40b69b36b5f027f1a4e48c135143848e30
james-zedd <james_zedd@hotmail.com>
2020-12-24 12:59:22 -0500

    Add components and SCSS files. Update main HTML page and manifesto

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <header>
+            <p>jameszedd.com</p>
+        </header>
+    )
+}
---
+++ src/components/Opener.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Opener = () => {
+    return (
+        <div>
+            <p>hello from opener</p>
+        </div>
+    )
+}
+
+
+
+
+
