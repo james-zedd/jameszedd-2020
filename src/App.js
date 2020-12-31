861694918a41b371927f65f09a7a327a14f312e8
james-zedd <james_zedd@hotmail.com>
2020-12-31 14:01:00 -0500

    Begin theming implementation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
 import "./scss/application.scss";
+
+import lightTheme from "./components/themes/light";
+import darkTheme from "./components/themes/dark";
 
 import { Header } from "./components/Header";
 import { Opener } from "./components/Opener";
@@ -9,21 +13,32 @@ import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 
 function App() {
-
-
+  const stored = localStorage.getItem("isDarkMode");
+  const [isDarkMode, setIsDarkMode] = useState(
+    stored === "true" ? true : false
+  );
   return (
-    <div id="theme" className="theme-default">
-      <div className="container">
-        <Header />
-        <main role="main">
-          <Opener />
-          <About />
-          <Work />
-          <Contact />
-        </main>
-        <Footer />
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <div id="theme" className="theme-default">
+        <button onClick={() => {
+          setIsDarkMode(!isDarkMode);
+          localStorage.setItem("isDarkMode", !isDarkMode);
+        }}>
+          Toggle Dark Mode
+        </button>
+        <div>Is dark mode: {isDarkMode}</div>
+        <div className="container">
+          <Header />
+          <main role="main">
+            <Opener />
+            <About />
+            <Work />
+            <Contact />
+          </main>
+          <Footer />
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
