315e63d2e32677a269b44acb14109267c0c4a12a
james-zedd <james.zedd@gmail.com>
2022-12-28 12:03:59 -0500

    Update opener, about and work sections. Adjust CSS as needed.

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import profilePic from "../assets/img/james_zedd__iaido__circle.png";
+import profilePic from '../assets/img/james_zedd__iaido__circle.png';
 
 export const About = () => {
     return (
-        <section className="about">
-            <div className="row">
-                <div className="about__text col col-6">
-                    <h1 className="about__title title">About</h1>
-                    <p>I have been a freelance web developer since 2015, specializing in Shopify theme development and web application development. I have built custom Shopify themes for clients from PSD mock ups and modified existing themes to client requests. I am also familiar with modern web application technologies, git and scrum/agile methodology.</p>
-                    <p>I began my professional career with a local software company in 2019 that builds and maintains Learning Management Software for enterprise-level clients.</p>
+        <section className='about'>
+            <div className='row'>
+                <div className='about__text col col-8'>
+                    <h1 className='about__title title'>About</h1>
+                    <p>
+                        I began my professional career with a local software
+                        company in 2019 that builds and maintains Learning
+                        Management Software for enterprise-level clients. In
+                        2021 I was promoted to Team Lead and I took on several
+                        additional responsibilities, including managing the
+                        goals of my team each sprint and deploying changes to
+                        our development environment.
+                    </p>
+                    <p>
+                        One great achievement I had in this role was being part
+                        of setting up an integration between a third-party
+                        software and our LMS software so that courses could be
+                        integrated into our software and the outcome of the
+                        course could be shared with the third-party.
+                    </p>
+                    <p>
+                        Before this position, I operated as a freelance web
+                        developer for four years, specializing in Shopify theme
+                        development. I have built custom Shopify themes for
+                        clients from PSD mock ups and modified existing themes
+                        to client requests.
+                    </p>
                 </div>
-                <div className="about__img col col-6 text-center">
-                    <img src={profilePic} className="about__profile-pic" alt="james_zedd"/>
+                <div className='about__img col col-4 text-center'>
+                    <img
+                        src={profilePic}
+                        className='about__profile-pic'
+                        alt='james_zedd'
+                    />
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import WorkItem from './WorkItem';
-import WorkItemFocus from "./WorkItemFocus";
+import WorkItemFocus from './WorkItemFocus';
 import Data from '../data/portfolio.json';
 
 function WorkItems(props) {
-    const items = props.items;
-    // console.log('items from work component', items);
-    return items.map(item => <WorkItem key={item.id} item={item} viewItem={props.viewItem} />);
+    const items = props.items.sort((a, b) => b.id - a.id);
+
+    return items.map((item) => (
+        <WorkItem key={item.id} item={item} viewItem={props.viewItem} />
+    ));
 }
 
 function getWorkItem(id) {
-    return Data.find(item => item.id === id);
+    return Data.find((item) => item.id === id);
 }
 
 export class Work extends Component {
@@ -19,37 +21,41 @@ export class Work extends Component {
         this.state = {
             activeItemId: '',
             activeItem: {},
-        }
+        };
     }
 
     viewItem = (id) => {
         this.setState({
             activeItemId: id,
-            activeItem: getWorkItem(id)
+            activeItem: getWorkItem(id),
         });
-    }
+    };
 
     clear = () => {
         this.setState({
             activeItemId: '',
-            activeItem: {}
+            activeItem: {},
         });
-    }
+    };
 
     render() {
         return (
-            <section className="work">
-                <h1 className="work__title title text-center">Work</h1>
-                <div className="work_item-focus">
-                    <WorkItemFocus activeItemId={this.state.activeItemId} activeItem={this.state.activeItem} clear={this.clear} />
+            <section className='work'>
+                <h1 className='work__title title text-center'>Work</h1>
+                <div className='work_item-focus'>
+                    <WorkItemFocus
+                        activeItemId={this.state.activeItemId}
+                        activeItem={this.state.activeItem}
+                        clear={this.clear}
+                    />
                 </div>
-                <div className="work__items">
-                    <div className="row">
+                <div className='work__items'>
+                    <div className='row'>
                         <WorkItems items={Data} viewItem={this.viewItem} />
                     </div>
                 </div>
             </section>
-        )
+        );
     }
 }
 
